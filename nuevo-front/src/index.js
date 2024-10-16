e4b1d94af87287f48274d81f6f2f6162f0c33854
james200327 <alexmatosmelo2@gmail.com>
2024-10-16 20:14:56 +0200

    subida

--- nuevo-front/src/index.js
+++ nuevo-front/src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import InicioSesion from './pantallas/InicioSesion';
-import PagInicio from './pantallas/PagInicio';
-
+//import PagInicio from './pantallas/PagInicio';
+import PagUsuario from './pantallas/PagUsuario';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
-    <PagInicio/>
+   
+    <PagUsuario/>
+
   </React.StrictMode>
 );
 
