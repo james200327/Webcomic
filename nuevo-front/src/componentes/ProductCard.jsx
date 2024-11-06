8d593548b313b9bd4afe84cd6b570c834c957ddb
JaviLL14 <javier.lopezlaureano@educa.madrid.org>
2024-11-06 14:02:04 +0100

    modificacion componentes ProductCard y ProductDetailPage para conectar con back

--- nuevo-front/src/componentes/ProductCard.jsx
+++ nuevo-front/src/componentes/ProductCard.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
-import imagen from '../Imagenes/aot/aot-1.jpg';
+import { useNavigate } from 'react-router-dom';
 import '../Estilos/ProductCard.css';
-export function ProductCard({image, comic , rating, reviews}) {
-  return (
-    <div className="product-card">
-      <img src={image} className="product-image" />
-      <h3 className="product-name">{comic.titulo}</h3>
-      <div className='div-product-price'>
-      <p className="product-price">{comic.precio}</p>
-      <img className='product-infinitypoints-img' src="/images/InfinityPoints.jpg" alt="InfinityPoints" />
-      </div>
-    
-      <div className="product-rating">
-        <span>{rating}/5 </span>
-        <span className='estrellas'>★</span>
-        <span>{reviews} opiniones</span>
-      </div>
-      <div className='div-delivery'>
-      <img src={image} alt="Infinity Comics" className='img-delivery' />
-      <p className="product-delivery">{comic.stock}</p>
-      </div>     
-    </div>
-  );
+
+export function ProductCard({ comic, image, rating, reviews }) {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        // Navega a ProductDetailPage pasando el ID del cómic en la URL
+        navigate(`/product/${comic.id}`);
+    };
+
+    return (
+        <div className="product-card" onClick={handleClick}>
+            <img src={image} alt={comic.titulo} />
+            <h2>{comic.titulo}</h2>
+            <p>Precio: {comic.precio}</p>
+            <p>Rating: {rating} ★</p>
+            <p>{reviews} Opiniones</p>
+            <p>Stock: {comic.stock}</p>
+        </div>
+    );
 }
-
-
-
