91ffbce0b413c324b8c348f75bf5cc4486110fbd
james200327 <alexmatosmelo2@gmail.com>
2024-11-26 21:01:49 +0100

    cambios en la pagina de usuario

--- nuevo-front/src/pantallas/UserProfile.jsx
+++ nuevo-front/src/pantallas/UserProfile.jsx
@@ -5,46 +5,56 @@ import '../Estilos/UserProfile.css';
 
 const UserProfile = () => {
     const navigate = useNavigate();
-    
+
     // Estado para almacenar los datos del usuario
     const [userData, setUserData] = useState({
         username: "",
         email: "",
-        contraseña: "",
-        direccion: "",
+        contrasenia: "",
+        calle: "",
+        codigoPostal: "",
+        ciudad: ""
     });
-    
+
     // Estado para saber si estamos editando o no
     const [isEditing, setIsEditing] = useState(false);
+    const [comics, setComics] = useState([]);
 
     // Llama al backend para obtener los datos del usuario cuando se monta el componente
     useEffect(() => {
         const fetchUserData = async () => {
             try {
                 const response = await axios.get(`http://localhost:8080/api/usuarios/${localStorage.getItem('userId')}`);
-                const user = response.data.usuario;  // Asegúrate de que 'usuario' es la propiedad correcta en la respuesta
-    
-                // Acceder a la dirección
-                const direccion = user.direccion;
-    
+                const user = response.data;  // Asegúrate de que 'usuario' es la propiedad correcta en la respuesta
+
                 // Actualizar el estado con los datos del usuario y la dirección
                 setUserData({
-                    nombre: user.nombre,  // Nombre del usuario
+                    username: user.username,  // Nombre del usuario
                     email: user.email,    // Email del usuario
-                    contraseña: user.password,  // Contraseña del usuario (si la necesitas mostrar)
-                    calle: direccion.calle,  // Calle de la dirección
-                    ciudad: direccion.ciudad,  // Ciudad de la dirección
-                    codigoPostal: direccion.codigoPostal  // Código postal de la dirección
+                    contrasenia: user.password,  // Contraseña del usuario (si la necesitas mostrar)
+                    calle: user.direccion.calle,  // Calle de la dirección
+                    ciudad: user.direccion.ciudad,  // Ciudad de la dirección
+                    codigoPostal: user.direccion.codigoPostal  // Código postal de la dirección
                 });
             } catch (error) {
                 console.error('Error al obtener los datos del usuario:', error);
             }
         };
-    
-        fetchUserData();
-    }, []);
-    
 
+        const fetchListaComics = async () => {
+            try {
+                const listaComics = await axios.get(`http://localhost:8080/api/pedidos/usuario/${localStorage.getItem('userId')}/confirmados`)
+                setComics(listaComics.data)
+
+            } catch (error) {
+                console.error('Error al obtener el listado de comics:', error);
+            }
+        }
+
+        fetchUserData();
+        fetchListaComics();
+    }, []);
+    console.log(comics)
     // Maneja el cambio de valores en los inputs del formulario
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -69,7 +79,7 @@ const UserProfile = () => {
     return (
         <div className="div-user">
             <h2>Perfil de Usuario</h2>
-            
+
             {isEditing ? (
                 <form onSubmit={handleSubmit} className="form-edit">
                     <div className="div-info">
@@ -141,7 +151,7 @@ const UserProfile = () => {
                     </div>
                     <div className="div-info">
                         <strong>Contraseña:</strong>
-                        <p>{userData.password}</p>
+                        <p>{userData.contrasenia}</p>
                     </div>
                     <div className="div-info">
                         <strong>Calle:</strong>
@@ -157,11 +167,38 @@ const UserProfile = () => {
                     </div>
                 </div>
             )}
-              
+
             <div className="div-buttons">
                 <button className="button-user" onClick={() => navigate('/inicio')}>Volver al Menú Principal</button>
                 <button className="button-user" onClick={() => setIsEditing(true)}>Modificar Datos</button>
             </div>
+
+
+            <div>
+                {comics.length > 0 ? (
+                    <div>
+                        {comics.map((pedido) => (
+                            /*Div donde se muestra el pedido id y el precio total del pedido*/
+                            <div key={pedido.id} className="pedido-card">
+                                <h3>Pedido ID: {pedido.id}</h3>
+                                <p><strong>Precio Total:</strong> {pedido.total} puntos</p>
+                                {/*Div que contiene las imagenes de los comics,titulo,cantindad de cada comic, precio que ha comoprado en este este pedido*/}
+                                <div className="pedido-comics">
+                                    {pedido.items.map((item) =>(
+                                        <><img src={`http://localhost:8080${item.comic.imagenUrl}`} alt={item.comic.titulo} />
+                                        <p><strong>{item.comic.titulo}</strong></p>
+                                        <p>Cantidad: {item.cantidad}</p>
+                                        <p>Subtotal: {item.cantidad * item.comic.precio} puntos</p></>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <p>No tienes pedidos confirmados aún.</p> // Muestra un mensaje si no hay pedidos
+                )}
+            </div>
+
         </div>
     );
 };
