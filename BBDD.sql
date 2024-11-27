c652fe81eb7a8ab25a572d599091417efa3fe2bf
JaviLL14 <javier.lopezlaureano@educa.madrid.org>
2024-11-27 21:10:27 +0100

    vacion rama main

--- BBDD.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'nombre_usuario'@'localhost' IDENTIFIED BY 'contraseña';
-create database webcomic;
-GRANT ALL PRIVILEGES ON webcomic.* TO 'nombre_usuario'@'localhost';
-FLUSH PRIVILEGES;
-use webcomic;
